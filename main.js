2bdb500fa559aae8713da769e1c2bbee77c358c2
Rez <probablyrez@gmail.com>
2023-09-23 12:11:09 +1000

    added slider

--- main.js
+++ main.js
@@ -2,10 +2,10 @@
 
 // console.log(window.innerWidth);
 
-let tileNumber = 20;
+let tileNumber = 10;
 
 function tileArea(tileColumns) {
-    let dimensions = (800 / (tileColumns+0.25)) - 5;
+    let dimensions = (790 / (tileColumns+0.25) - 5);
     let dRoot = document.querySelector(":root");
     let mainContainer = document.querySelector("div.main-container");
 
@@ -19,9 +19,20 @@ function tileArea(tileColumns) {
             let pixelRow = document.querySelectorAll("div.pixelrow");
             let pixelBox = `<div id="box${i}${j}" class="pixelbox unselected"></div>`;
             pixelRow[i].innerHTML += pixelBox;
+            
         }
         mainContainer.innerHTML += "</div>"
     }
 }
 
 tileArea(tileNumber);
+
+function clickHandler(e) {
+    // console.log(click);
+    // if (!click.target.classList.contains('pixelbox')) return;
+    // let cTarget = click.target.id;
+    console.log(e);
+}
+
+// window.addEventListener("click", clickHandler);
+// window.addEventListener("mousemove", clickHandler);
